e604d533cd074fc0993fc5086d067f7f5f3d9616
Joke Heyndels <jo_hannah@outlook.com>
2020-11-29 12:04:44 +0100

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookCatalog.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        
+        [Range(1, 10, ErrorMessage = "Rating must be between 1 and 10")]
+        public int Rating { get; set; }
+    }
+}
